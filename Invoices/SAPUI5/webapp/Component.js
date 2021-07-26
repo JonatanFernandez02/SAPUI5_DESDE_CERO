b148d70cba812b3776c460885cb929259f3f1d36
Jonatan Fernandez <jonatan.fernandez@globant.com>
2021-07-26 16:11:27 +0000

    FIN UNIDA 7, COMIENZA UNIDAD 8 DE ACA DEN ADELANTE

--- Invoices/SAPUI5/webapp/Component.js
+++ Invoices/SAPUI5/webapp/Component.js
@@ -1,21 +1,22 @@
 // @ts-nocheck
 sap.ui.define([
-    "sap/ui/core/UIComponent", 
+    "sap/ui/core/UIComponent",
     "logaligroup/SAPUI5/model/Models",
-    "sap/ui/model/resource/ResourceModel"
+    "sap/ui/model/resource/ResourceModel",
+    "./controller/HelloDialog"
 ],/**
  @param {typeof sap.ui.core.UIComponent} UIComponent 
 
 */
 
-    function (UIComponent,Models,ResourceModel) {
+    function (UIComponent, Models, ResourceModel, HelloDialog) {
         return UIComponent.extend("logaligroup.SAPUI5.Component", {
-            
+
             metadata: {
                 manifest: "json"
             },
-            
-            init : function () {
+
+            init: function () {
                 UIComponent.prototype.init.apply(this, arguments);
 
                 this.setModel(Models.createRecipient());
@@ -23,6 +24,15 @@ sap.ui.define([
                 var i18nModel = new ResourceModel({ bundleName: "logaligroup.SAPUI5.i18n.i18n" });
                 this.setModel(i18nModel, "i18n");
 
+                this._helloDialog = new HelloDialog(this.getRootControl());
+
+            },
+            exit: function () {
+                this._helloDialog.destroy();
+                delete this._helloDialog;
+            },
+            openHelloDialog : function(){
+             this._helloDialog.open();   
             }
         });
     });
