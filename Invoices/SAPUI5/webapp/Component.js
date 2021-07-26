be80af511d24120616f7f9aa72b9c19d24949767
Jonatan Fernandez <jonatan.fernandez@globant.com>
2021-07-26 12:32:16 +0000

    HASTA 5.14 en rama Unidad_5

---
+++ Invoices/SAPUI5/webapp/Component.js
@@ -0,0 +1,28 @@
+// @ts-nocheck
+sap.ui.define([
+    "sap/ui/core/UIComponent", 
+    "logaligroup/SAPUI5/model/Models",
+    "sap/ui/model/resource/ResourceModel"
+],/**
+ @param {typeof sap.ui.core.UIComponent} UIComponent 
+
+*/
+
+    function (UIComponent,Models,ResourceModel) {
+        return UIComponent.extend("logaligroup.SAPUI5.Component", {
+            
+            metadata: {
+                manifest: "json"
+            },
+            
+            init : function () {
+                UIComponent.prototype.init.apply(this, arguments);
+
+                this.setModel(Models.createRecipient());
+
+                var i18nModel = new ResourceModel({ bundleName: "logaligroup.SAPUI5.i18n.i18n" });
+                this.setModel(i18nModel, "i18n");
+
+            }
+        });
+    });
