f529076cd1ccd246d06ccb93ad5ccdc5d5445bcd
Jonatan Fernandez <jonatan.fernandez@globant.com>
2021-08-02 20:45:32 +0000

    FIN UNIDAD 14

--- Invoices/SAPUI5/webapp/Component.js
+++ Invoices/SAPUI5/webapp/Component.js
@@ -3,13 +3,15 @@ sap.ui.define([
     "sap/ui/core/UIComponent",
     "logaligroup/SAPUI5/model/Models",
     "sap/ui/model/resource/ResourceModel",
-    "./controller/HelloDialog"
+    "./controller/HelloDialog",
+    "sap/ui/Device"
 ],/**
  @param {typeof sap.ui.core.UIComponent} UIComponent 
-
+ @param {typeof sap.ui.model.resource.ResourceModel} ResourceModel 
+ @param {typeof sap.ui.Device} Device 
 */
 
-    function (UIComponent, Models, ResourceModel, HelloDialog) {
+    function (UIComponent, Models, ResourceModel, HelloDialog,Device) {
         return UIComponent.extend("logaligroup.SAPUI5.Component", {
 
             metadata: {
@@ -21,11 +23,15 @@ sap.ui.define([
                 //seteamos el datamodel en la vista
                 this.setModel(Models.createRecipient());
 
-               /*  var i18nModel = new ResourceModel({ bundleName: "logaligroup.SAPUI5.i18n.i18n" });
-                this.setModel(i18nModel, "i18n"); */
+                /*  var i18nModel = new ResourceModel({ bundleName: "logaligroup.SAPUI5.i18n.i18n" });
+                 this.setModel(i18nModel, "i18n"); */
+
+
+                //set the device Model
+                this.setModel(Models.createDeviceModel(), "device");
 
                 this._helloDialog = new HelloDialog(this.getRootControl());
-                    //create views based on the url / hash o pattern.
+                //create views based on the url / hash o pattern.
                 this.getRouter().initialize();
 
             },
@@ -33,8 +39,17 @@ sap.ui.define([
                 this._helloDialog.destroy();
                 delete this._helloDialog;
             },
-            openHelloDialog : function(){
-             this._helloDialog.open();   
+            openHelloDialog: function () {
+                this._helloDialog.open();
+            },
+            getContentDensityClass: function () {
+               if (!Device.support.touch){
+                   this._sContentDensityClass = "sapUiSizeCompact";
+               }
+               else{
+                         this._sContentDensityClass = "sapUiSizeCozy"
+               }
+               return this._sContentDensityClass
             }
         });
     });
