fe73797298fad1465bc23bb0b6456c3f28a9c62f
Jonatan Fernandez <jonatan.fernandez@globant.com>
2021-08-06 15:04:30 +0000

    FIN UNIDAD 15

--- Invoices/SAPUI5/webapp/controller/HelloPanel.controller.js
+++ Invoices/SAPUI5/webapp/controller/HelloPanel.controller.js
@@ -2,17 +2,19 @@
 
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/base/Log"
 
 ],
 
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller 
      * @param {typeof sap.m.MessageToast} MessageToast 
+     * @param {typeof sap.base.Log} Log 
 
    
      */
-    function (Controller, MessageToast) {
+    function (Controller, MessageToast, Log) {
         "use strict";
 
         return Controller.extend("logaligroup.SAPUI5.controller.HelloPanel", {
@@ -20,6 +22,17 @@ sap.ui.define([
             onInit: function () {
 
             },
+
+            onBeforeRendering: function () {
+                window.message = 'Log message - onBeforeRendering';
+                Log.info(window.message);
+                Log.error(window.message);
+
+            },
+           /*   onAfterRendering: function () {
+                debugger;
+            },  */
+
             onShowHello: function () {
                 var oBundle = this.getView().getModel("i18n").getResourceBundle();
                 var sRecipient = this.getView().getModel().getProperty("/recipient/name");
@@ -29,9 +42,9 @@ sap.ui.define([
             },
 
             onOpenDialog: function () {
-              this.getOwnerComponent().openHelloDialog();
+                this.getOwnerComponent().openHelloDialog();
             },
-            
+
 
 
 
